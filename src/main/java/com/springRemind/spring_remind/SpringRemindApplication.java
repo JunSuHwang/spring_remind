c6a89f65bbab2f7ead243cf18107475edc06b0d0
jsPC <wnstn9904@naver.com>
2025-02-05 00:00:20 +0900

    Initial commit

---
+++ src/main/java/com/springRemind/spring_remind/SpringRemindApplication.java
@@ -0,0 +1,13 @@
+package com.springRemind.spring_remind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRemindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springRemind/spring_remind/SpringRemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springRemind.spring_remind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
