6b0c4283847cbebcd508b42215beb43cc5176c5b
jsPC <wnstn9904@naver.com>
2025-03-06 17:58:39 +0900

    #05 API 인터페이스 및 결과 데이터 구조 설계

--- src/main/java/com/springRemind/spring_remind/controller/v1/UserController.java
+++ src/main/java/com/springRemind/spring_remind/controller/v1/UserController.java
@@ -1,14 +1,16 @@
 package com.springRemind.spring_remind.controller.v1;
 
 import com.springRemind.spring_remind.entity.User;
+import com.springRemind.spring_remind.model.response.CommonResult;
+import com.springRemind.spring_remind.model.response.ListResult;
+import com.springRemind.spring_remind.model.response.SingleResult;
 import com.springRemind.spring_remind.repo.UserJpaRepo;
+import com.springRemind.spring_remind.service.ResponseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "UserController", description = "1. User")
 @RequiredArgsConstructor
@@ -16,16 +18,25 @@ import java.util.List;
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
+    private final ResponseService responseService;
 
     @Operation(summary = "회원 조회", description = "모든 회원을 조회한다.")
     @GetMapping(value = "/user")
-    public List<User> findAllUser() {
-        return userJpaRepo.findAll();
+    public ListResult<User> findAllUser() {
+        return responseService.getListResult(userJpaRepo.findAll());
+    }
+
+    @Operation(summary = "회원 단건 조회", description = "userId로 회원을 조회한다.")
+    @GetMapping(value = "/user/{msrl}")
+    public SingleResult<User> findAllUser(
+            @Parameter(name = "msrl", description = "회원번호",required = true) @PathVariable("msrl") long msrl
+    ) {
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
     }
 
     @Operation(summary = "회원 입력", description = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save(
+    public SingleResult<User> save(
             @Parameter(name = "uid", description = "회원아이디",required = true) @RequestParam(value = "uid") String uid,
             @Parameter(name = "name", description = "회원이름",required = true) @RequestParam(value = "name")String name
     ) {
@@ -33,6 +44,30 @@ public class UserController {
                 .uid(uid)
                 .name(name)
                 .build();
-        return userJpaRepo.save(user);
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @Operation(summary = "회원 수정", description = "회원정보를 수정한다.")
+    @PutMapping(value = "/user")
+    public SingleResult<User> modify(
+            @Parameter(name = "msrl", description = "회원번호",required = true) @RequestParam(value = "msrl") long msrl,
+            @Parameter(name = "uid", description = "회원아이디",required = true) @RequestParam(value = "uid") String uid,
+            @Parameter(name = "name", description = "회원이름",required = true) @RequestParam(value = "name")String name
+    ) {
+        User user = User.builder()
+                .msrl(msrl)
+                .uid(uid)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @Operation(summary = "회원 삭제", description = "회원번호로 회원정보를 삭제한다.")
+    @PutMapping(value = "/user/{msrl}")
+    public CommonResult delete(
+            @Parameter(name = "msrl", description = "회원번호",required = true) @PathVariable("msrl") long msrl
+    ) {
+        userJpaRepo.deleteById(msrl);
+        return responseService.getSuccessResult();
     }
 }
---
+++ src/main/java/com/springRemind/spring_remind/model/response/CommonResult.java
@@ -0,0 +1,19 @@
+package com.springRemind.spring_remind.model.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommonResult {
+
+    @Schema(description = "응답 성공여부 : true/false")
+    private boolean success;
+
+    @Schema(description = "응답 코드 번호 : >= 0 정상, < 0 비정상")
+    private int code;
+
+    @Schema(description = "응답 메세지")
+    private String msg;
+}
